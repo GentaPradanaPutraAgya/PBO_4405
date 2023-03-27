91dfee7d404ee6f33052bcfef097ccba16b4b0db
GentaPradanaPutraAgya <92007199+GentaPradanaPutraAgya@users.noreply.github.com>
2023-03-27 22:50:58 +0700

    Add files via upload

---
+++ minggu 4/13499/hitungDataMhs.java
@@ -0,0 +1,7 @@
+public class hitungDataMhs{
+    public static void main(String args[]){
+        DataMhs mhs1 = new DataMhs();
+        mhs1.Datanilai("A11.2021.13499", "Genta Pradana Putra Agya", 80, 90 ,87);
+        mhs1.tampilData();
+    }
+}

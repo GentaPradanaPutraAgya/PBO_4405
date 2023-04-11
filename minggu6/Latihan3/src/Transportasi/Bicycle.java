729fa7cb3394dd42b35b76df3fa8bcf0fe428fd7
GentaPradanaPutraAgya <92007199+GentaPradanaPutraAgya@users.noreply.github.com>
2023-04-11 23:57:51 +0700

    Add files via upload

---
+++ minggu6/Latihan3/src/Transportasi/Bicycle.java
@@ -0,0 +1,7 @@
+package Transportasi;
+
+public class Bicycle {
+    public void cetak2() {
+        System.out.println("Sepeda lipat");
+    }
+}
---
+++ minggu6/Latihan3/src/Transportasi/Mobil.java
@@ -0,0 +1,7 @@
+package Transportasi;
+
+public class Mobil {
+    public void cetak() {
+        System.out.println("Mobil sport");
+    }
+}
